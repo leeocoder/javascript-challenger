68731433a56be023d1a6d2e8f5f3991a2ebcd6ae
Leonardo Albuquerque <leonardo@macrolabs.com.br>
2023-10-29 12:38:27 -0300

    Solve challenge

--- junior.js
+++ junior.js
@@ -1,26 +1,45 @@
 //Crie uma função que receba uma string e retorne a mesma string, mas com todas as letras em maiúsculas.
-const toUpperCase = (str) => {};
+const toUpperCase = (str) => {
+  return str.toUpperCase();
+};
 
 //Escreva uma função que recebe duas strings como argumento e concatena-as em uma única string com um espaço entre elas.
-const concatStrings = (str1, str2) => {};
+const concatStrings = (str1, str2) => {
+  return `${str1}${str2}`;
+  // return [str1, str2].join('');
+};
 
 //Crie uma função que recebe uma string como argumento e retorna o número de caracteres nessa string.
-const numberString = (str) => {};
+const numberString = (str) => {
+  return str.length;
+  // return str.split('').length;
+};
 
 //Crie uma função que receba uma palavra, e uma frase. Você deve verificar se existe esta palavra nesta frase e retornar true ou false.
-const existKeyword = (keyword, str) => {};
+const existKeyword = (keyword, str) => {
+  return str.split(' ').includes(keyword);
+};
 
 //Escreva uma função que recebe um array de números como argumento e retorna a soma de todos os números no array.
-const sumAllItemsInArray = (arr) => {};
+const sumAllItemsInArray = (arr) => {
+  const initialValue = 0;
+  return arr.reduce((previousValue, currentValue) => previousValue + currentValue, initialValue);
+};
 
 //Você receberá uma lista com nomes de usuarios, verifique se um usuario existe neste array e retorne true ou false.
-const verifyUserInArray = (strArr, str) => {};
+const verifyUserInArray = (strArr, str) => {
+  return strArr.includes(str);
+};
 
 //Escreva uma função que recebe dois números como argumento e retorna a soma deles.
-const sumNumber = (num1, num2) => {};
+const sumNumber = (num1, num2) => {
+  return num1 + num2;
+};
 
 //Crie uma função que recebe um número como argumento e verifica se ele é par ou ímpar retornando  a string "par" ou "impar"
-const verifyNumberIsPairOrOdd = (num) => {};
+const verifyNumberIsPairOrOdd = (num) => {
+  return num % 2 === 0 ? 'par' : 'impar';
+};
 
 module.exports = {
   toUpperCase,
